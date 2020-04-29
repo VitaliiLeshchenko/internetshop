aedfc4ac071cb325de7eeeb05d84c23106bee5ce
VitaliiLeshchenko <411100253g@gmail.com>
2020-04-29 12:22:23 +0300

    frontAnd changed

--- src/main/java/mate/academy/internetshop/controllers/AddProductToBucketController.java
+++ src/main/java/mate/academy/internetshop/controllers/AddProductToBucketController.java
@@ -6,23 +6,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.academy.internetshop.lib.Injector;
-import mate.academy.internetshop.model.Product;
 import mate.academy.internetshop.service.ProductService;
 import mate.academy.internetshop.service.ShoppingCartService;
 
-public class ProductDeleteFromBucketController extends HttpServlet {
+public class AddProductToBucketController extends HttpServlet {
     private static final Long USER_ID = 1L;
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private ShoppingCartService shoppingCartService
-            = (ShoppingCartService)INJECTOR.getInstance(ShoppingCartService.class);
+            = (ShoppingCartService) INJECTOR.getInstance(ShoppingCartService.class);
     private ProductService productService
-            = (ProductService)INJECTOR.getInstance(ProductService.class);
+            = (ProductService) INJECTOR.getInstance(ProductService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        Product product = productService.get(Long.parseLong(req.getParameter("productId")));
-        shoppingCartService.deleteProduct(shoppingCartService.getByUserId(USER_ID), product);
-        resp.sendRedirect(req.getContextPath() + "/shoppingCartShow");
+        shoppingCartService.addProduct(shoppingCartService.getByUserId(USER_ID),
+                productService.get(Long.parseLong(req.getParameter("productId"))));
+        resp.sendRedirect(req.getContextPath() + "/productList");
     }
 }
--- src/main/java/mate/academy/internetshop/controllers/DeleteProductFromBucketController.java
+++ src/main/java/mate/academy/internetshop/controllers/DeleteProductFromBucketController.java
@@ -6,22 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.academy.internetshop.lib.Injector;
+import mate.academy.internetshop.model.Product;
 import mate.academy.internetshop.service.ProductService;
 import mate.academy.internetshop.service.ShoppingCartService;
 
-public class ProductAddToBucketController extends HttpServlet {
+public class DeleteProductFromBucketController extends HttpServlet {
     private static final Long USER_ID = 1L;
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private ShoppingCartService shoppingCartService
-            = (ShoppingCartService) INJECTOR.getInstance(ShoppingCartService.class);
+            = (ShoppingCartService)INJECTOR.getInstance(ShoppingCartService.class);
     private ProductService productService
-            = (ProductService) INJECTOR.getInstance(ProductService.class);
+            = (ProductService)INJECTOR.getInstance(ProductService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        shoppingCartService.addProduct(shoppingCartService.getByUserId(USER_ID),
-                productService.get(Long.parseLong(req.getParameter("productId"))));
-        resp.sendRedirect(req.getContextPath() + "/productList");
+        Product product = productService.get(Long.parseLong(req.getParameter("productId")));
+        shoppingCartService.deleteProduct(shoppingCartService.getByUserId(USER_ID), product);
+        resp.sendRedirect(req.getContextPath() + "/shoppingCartShow");
     }
 }
