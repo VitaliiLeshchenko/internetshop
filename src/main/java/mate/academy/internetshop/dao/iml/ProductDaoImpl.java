d37d9b8964463fbcfe80812c302191e708d22e86
Vitalii <59371243+VitaliiLeshchenko@users.noreply.github.com>
2020-05-12 11:11:47 +0300

    Hw mysql (#11)
    * added newDAO, exception, init_db.sql, ConnectionUtil
    
    * little changes (renamed, style refactored etc.).
    
    * removed logs

--- src/main/java/mate/academy/internetshop/dao/iml/ProductDaoImpl.java
+++ src/main/java/mate/academy/internetshop/dao/iml/ProductDaoImpl.java
@@ -3,12 +3,10 @@ package mate.academy.internetshop.dao.iml;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
-import mate.academy.internetshop.dao.Dao;
 import mate.academy.internetshop.dao.ProductDao;
 import mate.academy.internetshop.db.Storage;
 import mate.academy.internetshop.model.Product;
 
-@Dao
 public class ProductDaoImpl implements ProductDao {
 
     @Override
